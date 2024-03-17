716685bc91ac8d656934f3f97f45e5bec06a1304
Aditya <adi101197sahu@gmail.com>
2024-03-17 16:00:18 +0530

    create api for sign in

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res,next) => {
   const { username, email, password } = req.body;
@@ -31,3 +32,40 @@ export const signup = async (req, res,next) => {
     next(error);
   }
 };
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === '' || password === '') {
+    next(errorHandler(400, 'All fields are required'));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, 'User not found'));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, 'Invalid password'));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+//The first argument to jwt.sign() is the payload of the token, which typically contains data you want to store in the token. In this case, it seems to include the user's ID.
+//The second argument is the secret key used to sign the token. It's retrieved from the environment variable process.env.JWT_SECRET.
+//It sets the status code to 200 indicating success.
+// It sets a cookie named access_token with the JWT token as its value. The cookie is configured with the httpOnly option set to true, which means it can only be accessed by the server and not by client-side scripts, enhancing security.
+// Finally, it sends a JSON response (json(rest)) containing the user data (rest) except for the password field.
